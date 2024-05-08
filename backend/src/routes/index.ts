d05fe253e5706156d99ed77d65573ce98fa3fb59
Gage <77414756+Royuin@users.noreply.github.com>
2024-05-07 23:08:57 -0500

    Refactored get home function req, res, and next to have type of their corresponding express functions.

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,7 +1,7 @@
-const express = require('express');
+import express, {Request, Response, NextFunction} from 'express';
 const router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/', function(req:Request, res:Response, next:NextFunction) {
   res.render('index', { title: 'Blog' });
 });
 
