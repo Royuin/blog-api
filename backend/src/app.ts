4706c63053e76b76ab88d1666d496e4d9cd6246c
Gage <77414756+Royuin@users.noreply.github.com>
2024-05-11 21:31:13 -0500

    Added cors a dependency and made express use it

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import express, {Request, Response, NextFunction, ErrorRequestHandler} from 'exp
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 
@@ -12,6 +13,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use('/', indexRouter);
 
