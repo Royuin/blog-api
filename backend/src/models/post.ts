9b0d63a65e0ae9a33a35f162d1a0abfc9370c2a9
Gage <77414756+Royuin@users.noreply.github.com>
2024-05-14 21:30:45 -0500

    Fixed posts and comments keys of users and posts being a type of reference to models instead of Array type.

--- backend/src/models/post.ts
+++ backend/src/models/post.ts
@@ -3,14 +3,15 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const User = require('./user');
 const Comment = require('./comment');
+const ObjectId = mongoose;
 
 const PostSchema = new Schema ({
   title: { required: true, minLength: 3, type: String },
   content: { required: true, type: String },
   author: { required: true, type: Schema.Types.ObjectId, ref: 'User'},
   postDate: { required: true, type: Date },
-  comments: { type: [Comment], default: undefined },
-  isPublic: { required: true, type: Boolean },
+  comments: { type: Array },
+  isPublic: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model('Post', PostSchema);
--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -2,12 +2,13 @@ export {};
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const Post = require('./post');
+const ObjectId = mongoose;
 
 const UserSchema = new Schema ({
   username: { required: true, type: String, minLength: 3}, 
   password: { required: true, type: String, minLength: 4},
-  posts: { type: [Post], default: undefined},
-  isAuthor: { required: true, default: false },
+  posts: { type: Array },
+  isAuthor: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model('User', UserSchema);
