0d92424351a68de179e3ed45e1a09b58065a1df4
Gage <77414756+Royuin@users.noreply.github.com>
2024-05-15 21:27:27 -0500

    Made home route pass all posts as json.

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,14 @@
 import express, {Request, Response, NextFunction} from 'express';
 const router = express.Router();
+const Post = require('../models/post');
 
-router.get('/', function(req:Request, res:Response, next:NextFunction) {
-  res.render('index', { title: 'Blog' });
+router.get('/', async function(req:Request, res:Response, next:NextFunction) {
+  const posts = await Post.find();
+  const postsJson = JSON.stringify(posts);
+  res.json({
+    "title": "Blog",
+    "posts": postsJson,
+  });
 });
 
 module.exports = router;
