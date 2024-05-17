d112cead1ec67a81824e620feb97acd4db865e80
Gage <77414756+Royuin@users.noreply.github.com>
2024-05-16 21:42:59 -0500

    Added a route for sending all posts as json.

--- backend/src/routes/post.ts
+++ backend/src/routes/post.ts
@@ -5,7 +5,14 @@ const Post = require('../models/post');
 router.get('/post/:id', async (req:Request, res:Response, next:NextFunction) => {
   const post = await Post.findById(req.params.id);
   res.json({
-    "post": post,
+    post
+  });
+});
+
+router.get('/posts/', async (req:Request, res:Response, next:NextFunction) => {
+  const allPosts = await Post.find();
+  res.json({
+    allPosts
   });
 });
 
