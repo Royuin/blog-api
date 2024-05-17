e12f1e52ee56a458c95b028e3bc496c991c61f2b
Gage <77414756+Royuin@users.noreply.github.com>
2024-05-16 22:52:10 -0500

    Made get post function also give comments of post with post json.

--- backend/src/controllers/postController.ts
+++ backend/src/controllers/postController.ts
@@ -1,14 +1,18 @@
 import {Request, Response, NextFunction } from 'express';
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
-exports.getPost( async (req:Request, res:Response, next:NextFunction) => {
-  const post = await Post.findById(req.params.productId);
+exports.getPost = ( async (req:Request, res:Response, next:NextFunction) => {
+  const post = await Post.findById(req.params.id);
+  const commentsIds = post.comments;
+  const comments = await Comment.find({'_id': {$in: commentsIds}}).exec(); 
   res.json({
-    "post": post,
+    post,
+    comments
   });
 });
 
-exports.allPosts( async (req:Request, res:Response, next:NextFunction) => {
+exports.allPosts = ( async (req:Request, res:Response, next:NextFunction) => {
   const allPosts = await Post.find();
   res.json({
     "allPosts": allPosts,
