ace85b50a59603b4d37129f77e0dc789747ed7d6
Gage <77414756+Royuin@users.noreply.github.com>
2024-05-16 22:53:32 -0500

    Refactored route for getting post by id to use postController getPost function.

--- backend/src/routes/post.ts
+++ backend/src/routes/post.ts
@@ -1,13 +1,10 @@
 import express, {Request, Response, NextFunction} from 'express';
 const router = express.Router();
 const Post = require('../models/post');
+const Comment = require('../models/comment');
+const postController = require('../controllers/postController');
 
-router.get('/post/:id', async (req:Request, res:Response, next:NextFunction) => {
-  const post = await Post.findById(req.params.id);
-  res.json({
-    post
-  });
-});
+router.get('/post/:id', postController.getPost);
 
 router.get('/posts/', async (req:Request, res:Response, next:NextFunction) => {
   const allPosts = await Post.find();
