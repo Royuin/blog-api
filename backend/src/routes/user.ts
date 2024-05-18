4e8fb79a05ea1e76451e738a18e95a9dd48ecddc
Gage <77414756+Royuin@users.noreply.github.com>
2024-05-17 22:46:53 -0500

    Made a route for showing all users.

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,6 +2,8 @@ import express, {Request, Response, NextFunction} from 'express';
 const router = express.Router();
 const userController = require('../controllers/userController');
 
+router.get('/users', userController.allUsers);
+
 router.get('/user/:id', userController.getUser);
 
 module.exports = router;
