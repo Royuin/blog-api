da2597d64a486c23130fe5e8f2ae15db7fefd0bb
Gage <77414756+Royuin@users.noreply.github.com>
2024-05-17 22:44:19 -0500

    Made a user router and set up a route for a specific user by id.

---
+++ backend/src/routes/user.ts
@@ -0,0 +1,7 @@
+import express, {Request, Response, NextFunction} from 'express';
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+router.get('/user/:id', userController.getUser);
+
+module.exports = router;
