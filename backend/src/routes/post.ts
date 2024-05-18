e2e87bc8d7e46ad27d716251277c2d74b5d1e6a6
Gage <77414756+Royuin@users.noreply.github.com>
2024-05-17 21:33:35 -0500

    Removed unused import.

--- backend/src/routes/post.ts
+++ backend/src/routes/post.ts
@@ -1,7 +1,5 @@
 import express, {Request, Response, NextFunction} from 'express';
 const router = express.Router();
-const Post = require('../models/post');
-const Comment = require('../models/comment');
 const postController = require('../controllers/postController');
 
 router.get('/post/:id', postController.getPost);
