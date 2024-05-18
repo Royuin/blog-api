335135eeacf508d1a5f16029eb18436ab6f013fb
Gage <77414756+Royuin@users.noreply.github.com>
2024-05-17 22:46:33 -0500

    Made function to send all users as json.

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -2,6 +2,13 @@ import {Request, Response, NextFunction } from 'express';
 const User = require('../models/user');
 const Post = require('../models/post');
 
+exports.allUsers = ( async (req:Request, res:Response, next:NextFunction) => {
+  const allUsers = await User.find();
+  res.json({
+    allUsers
+  });
+});
+
 exports.getUser = ( async (req:Request, res:Response, next:NextFunction) => {
   const user = await User.findById(req.params.id);
   const posts = await Post.find({'_id': { $in: user.posts }}).exec();
