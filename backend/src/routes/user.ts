e4d589bc84734efe6235dd6413175e335d57b783
Gage <77414756+Royuin@users.noreply.github.com>
2024-05-19 11:59:05 -0500

    Made a post route for login that authenticates user.

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,6 +1,9 @@
 import express, {Request, Response, NextFunction} from 'express';
 const router = express.Router();
 const userController = require('../controllers/userController');
+const passport = require('passport');
+
+router.post('/login', passport.authenticate('local', {successRedirect: '/', failure: '/login' }));
 
 router.get('/users', userController.allUsers);
 
