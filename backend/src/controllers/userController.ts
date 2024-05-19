29f277a5665f2b66f492c697ae36a620b0235783
Gage <77414756+Royuin@users.noreply.github.com>
2024-05-19 10:43:56 -0500

    Made a function to handle posts request for users signing up.

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,6 +1,36 @@
 import {Request, Response, NextFunction } from 'express';
 const User = require('../models/user');
 const Post = require('../models/post');
+import { query, validationResult } from 'express-validator';
+const genPassword = require('../utils/passwordUtils');
+
+exports.signupPost = [
+  query('username', 'Username must not be empty and must be at least 3 characters.').trim().notEmpty().isLength({min: 3, max: 20}).escape(),
+  query('password', 'Password must not be empty and must be at least 4 characters.').trim().notEmpty().isLength({min: 4, max: 20}).escape(),
+
+  async (req:Request, res:Response, next:NextFunction) => {
+    const errors = validationResult(req);
+    const userExists = await User.findOne({username: req.query.username});
+
+    if (!errors.isEmpty()) {
+      res.json({
+        errors
+      });
+      return;
+    } else if (userExists) {
+      res.json({
+        userExists,
+      });
+      return;
+    } else {
+      const hashedPassword = genPassword(req.query.password);
+      const newUser = new User(req.query.username, hashedPassword, [], req.query.isAuthor);
+      await newUser.save();
+
+      res.redirect('/');
+    };
+  },
+];
 
 exports.allUsers = ( async (req:Request, res:Response, next:NextFunction) => {
   const allUsers = await User.find().exec();
