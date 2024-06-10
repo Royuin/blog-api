a41e6165b335783b2f0a29e48d608b39f6795dc8
Gage <77414756+Royuin@users.noreply.github.com.>
2024-06-09 19:23:34 -0500

    Made function to get all posts of a given user and return as json.

--- backend/src/controllers/postController.ts
+++ backend/src/controllers/postController.ts
@@ -1,5 +1,6 @@
 import {Request, Response, NextFunction } from 'express';
 const Post = require('../models/post');
+const User = require('../models/user');
 const Comment = require('../models/comment');
 
 exports.getPost = ( async (req:Request, res:Response, next:NextFunction) => {
@@ -13,7 +14,18 @@ exports.getPost = ( async (req:Request, res:Response, next:NextFunction) => {
 
 exports.allPosts = ( async (req:Request, res:Response, next:NextFunction) => {
   const allPosts = await Post.find().exec();
+  console.log(allPosts);
   res.json({
     "allPosts": allPosts,
+  });
+});
+
+exports.userPosts = ( async (req:Request, res:Response, next:NextFunction) => {
+  const user = await User.findById(req.params.id).exec();
+  const userPosts = await Post.find({author: user}).exec();
+
+  res.json({
+    userPosts,
+    user
   });
 });
