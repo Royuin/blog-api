39c15407f7cea23d13fad21b3bc527200791161e
Gage <77414756+Royuin@users.noreply.github.com.>
2024-06-30 14:26:47 -0500

    Made a create post function that verifies that user is logged in with a valid token.

--- backend/src/controllers/postController.ts
+++ backend/src/controllers/postController.ts
@@ -2,6 +2,9 @@ import {Request, Response, NextFunction } from 'express';
 const Post = require('../models/post');
 const User = require('../models/user');
 const Comment = require('../models/comment');
+import { query, validationResult } from 'express-validator';
+import passport from 'passport';
+const { verifyToken } = require('../utils/tokenUtils');
 
 exports.getPost = ( async (req:Request, res:Response, next:NextFunction) => {
   const post = await Post.findById(req.params.id).exec();
@@ -29,3 +32,15 @@ exports.userPosts = ( async (req:Request, res:Response, next:NextFunction) => {
     user
   });
 });
+
+exports.createPost = [
+  query('title', 'Title must not be empty.').trim().notEmpty().escape(),
+  query('content', 'Content must not be empty and be at least 50 characters long.').trim().isLength({min: 50}).escape(),
+  query('isPublic').escape(), 
+
+  async (req:Request, res:Response, next:NextFunction) => {
+    
+  verifyToken();
+  },
+]
+
