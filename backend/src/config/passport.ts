571f457c6a720aad70ef2d8a217a1e8b774e0cfc
Gage <77414756+Royuin@users.noreply.github.com.>
2024-07-09 21:51:45 -0500

    Configured strategy for passport-jwt.

--- backend/src/config/passport.ts
+++ backend/src/config/passport.ts
@@ -4,6 +4,13 @@ import { ObjectId } from 'mongoose';
 import * as passportLocal from 'passport-local';
 const LocalStrategy = passportLocal.Strategy;
 const { validatePassword } = require('../utils/passwordUtils');
+ const JwtStrategy = require('passport-jwt').Strategy,
+   ExtractJwt = require('passport-jwt').ExtractJwt;
+
+interface jwt_payload {
+  role: string;
+  sub: string;
+}
 
 interface userDocument {
   username: string;
@@ -12,7 +19,13 @@ interface userDocument {
   isAuthor: boolean;
 };
 
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET,
+};
+
 passport.use(new LocalStrategy(function verify(username:string, password:string, done) {
+  console.log('I AM USING AUTHENTICATE');
   User.find({username}).exec(), async function (err:unknown, user:userDocument) {
     if (err) { return done(err); }
     if (!user) { return done(null, false, { message: 'Incorrect username.'}); }
@@ -22,11 +35,27 @@ passport.use(new LocalStrategy(function verify(username:string, password:string,
     if(isValid) {
       return done(null, user);
     } else if (!isValid) {
+      console.log('Is not valid');
       return done(err);
     }
 
     if (err) {
+      console.log('Random error');
       done(err);
     };
   }
+}));
+
+// done set to any type because it is a callback function from passport library and @types was not fixing done having implicity of any
+passport.use(new JwtStrategy(opts, function(jwt_payload:jwt_payload, done:any) {
+  User.findOne({id: jwt_payload.sub}, function(err:unknown, user:userDocument) {
+    if (err) {
+      return done(err, false);
+    }
+    if (user) {
+      return done(null, user);
+    } else {
+      return done(null, false);
+    }
+  });
 }));
